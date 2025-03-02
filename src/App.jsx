555734f83e9ae409e5f6c227ec37f3cc49a48d96
inhtruong <duydat.dhsp@gmail.com>
2025-03-02 20:59:08 +0700

    Configure Routing in a React App

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* http://localhost:3000 */}
+          <Route path='/' element={<ListEmployeeComponent />}></Route>
+          {/* http://localhost:3000/employees */}
+          <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
